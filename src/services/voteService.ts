25c8c0838bfc4e1641de1a42189f7b4ff8f5f972
Comagol <comabellafrancisco@gmail.com>
2025-09-08 19:14:37 -0300

    Implement vote service methods for retrieving match stats, winner, total votes, validating votes, and creating votes in TypeScript

---
+++ src/services/voteService.ts
@@ -0,0 +1,36 @@
+import api from './api';
+import type { Vote } from '../types/index';
+
+export const voteService = {
+  //Rutas Publicas
+  //get match stats
+  getMatchStats: async (matchId: string): Promise<Vote[]> => {
+    const response = await api.get(`/votes/${matchId}/stats`);
+    return response.data;
+  },
+
+  //get ganador del partido
+  getMatchWinner: async (matchId: string): Promise<any> => {
+    const response = await api.get(`/votes/${matchId}/winner`);
+    response.data;
+  },
+
+  //Get total votos
+  getTotalVotes: async (MatchId: string): Promise<number> => {
+    const response = await api.get(`/votes/${MatchId}/total-votes`);
+    return response.data;
+  },
+
+  //Rutas protegidas (usuario autenticado)
+  //Validar voto
+  validateVote: async (matchId: string): Promise<boolean> => {
+    const response = await api.get(`/votes/validate/${matchId}`);
+    return response.data;
+  },
+
+  //Realizar un voto
+  createVote: async (playerId: string, matchId: string): Promise<Vote> => {
+    const response = await api.post(`/votes/`, { playerId, matchId });
+    return response.data;
+  }
+};
