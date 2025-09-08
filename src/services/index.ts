565e294555a0ee851f52732a1223b55f01897e96
Comagol <comabellafrancisco@gmail.com>
2025-09-08 19:26:26 -0300

    Add index file to export services for authentication, matching, voting, user management, and admin functionalities

---
+++ src/services/index.ts
@@ -0,0 +1,6 @@
+export { authService } from './authService';
+export { matchService } from './matchService';
+export { voteService } from './voteService';
+export { userService } from './userService';
+export { adminService } from './adminService';
+export { default as api } from './api';
