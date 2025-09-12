67f33e3509999473345a1b54a169e070693edd8a
Comagol <comabellafrancisco@gmail.com>
2025-09-12 11:21:43 -0300

    Add useMatches hook for managing match data retrieval, including loading state and error handling

---
+++ src/hooks/useMatches.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import { matchService } from "../services";
+import type { Match } from "../types";
+
+export const useMatches = () => {
+  const [matches, setMatches] = usetState<Match[]>([]);
+  const [activeMatches, setActiveMatches] = useState<Match[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  //obtengo los partidos
+  const fectchAllMatches = async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const allMatches = await matchService.getAllMatches();
+      setMatches(allMatches);
+      return allMatches;
+    } catch (err: any) {
+      setError(err.response?.data?.message || 'Error al obtener todos los partidos');
+      throw err;
+    }finally {
+      setIsLoading(false);
+    }
+  };
+
+  //obtengo los partidos activos
+  const fetchActiveMatches = async () => {
+    
+  }
+}
