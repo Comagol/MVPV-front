85e14978db125ac6fb0c9a688a4e3b87fd05dd6b
Comagol <comabellafrancisco@gmail.com>
2025-09-17 22:52:30 -0300

    Add playerService export to index for enhanced player management integration.

--- src/services/index.ts
+++ src/services/index.ts
@@ -3,4 +3,5 @@ export { matchService } from './matchService';
 export { voteService } from './voteService';
 export { userService } from './userService';
 export { adminService } from './adminService';
+export { playerService } from './playerService';
 export { default as api } from './api';
