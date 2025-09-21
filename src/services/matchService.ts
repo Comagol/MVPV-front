90b05954e2d7fe892bbdd2b80c275b6428021792
Comagol <comabellafrancisco@gmail.com>
2025-09-21 19:24:06 -0300

    Add matchService methods to return MatchResponse instead of Match, updating type references for improved clarity and consistency.

--- src/services/matchService.ts
+++ src/services/matchService.ts
@@ -1,49 +1,49 @@
 import api from './api';
-import type { Match } from '../types/index';
+import type { MatchResponse, CreateMatchRequest } from '../types/index';
 
 export const matchService = {
   //Rutas publicas
   //get all matches
-  getAllMatches: async (): Promise<Match[]> => {
+  getAllMatches: async (): Promise<MatchResponse[]> => {
     const response = await api.get('/matches/');
-    return response.data;
+    return response.data.match;
   },
 
   //get partidos activos
-  getActiveMatches: async (): Promise<Match[]> => {
+  getActiveMatches: async (): Promise<MatchResponse[]> => {
     const response = await api.get('/matches/active/matches');
-    return response.data;
+    return response.data.match;
   },
 
   //Get match By ID
-  getMatchById: async (id: string): Promise<Match> => {
+  getMatchById: async (id: string): Promise<MatchResponse> => {
     const response = await api.get(`/matches/${id}`);
-    return response.data;
+    return response.data.match;
   },
 
   //rutas protegidas
   //create match
-  createMatch: async (matchData: Omit<Match, '_id'>): Promise<Match> => {
+  createMatch: async (matchData: CreateMatchRequest): Promise<MatchResponse> => {
     const response = await api.post('/matches/', matchData);
-    return response.data;
+    return response.data.match;
   },
 
   //update Match
-  updateMatch: async (id: string, matchData: Partial<Match>): Promise<Match> => {
+  updateMatch: async (id: string, matchData: Partial<MatchResponse>): Promise<MatchResponse> => {
     const response = await api.put(`/matches/${id}`, matchData);
-    return response.data;
+    return response.data.match;
   },
 
   //start match
-  startMatch: async (id: string): Promise<Match> => {
+  startMatch: async (id: string): Promise<MatchResponse> => {
     const response = await api.put(`/matches/${id}/start`);
-    return response.data;
+    return response.data.match;
   },
 
   //finish match
-  finishMatch: async (id: string): Promise<Match> => {
+  finishMatch: async (id: string): Promise<MatchResponse> => {
     const response = await api.put(`/matches/${id}/finish`);
-    return response.data;
+    return response.data.match;
   }
 
 };
