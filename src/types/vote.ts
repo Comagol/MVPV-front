f4faf68e8fba5ddfb3638133c93982b77f3244e2
Comagol <comabellafrancisco@gmail.com>
2025-09-21 23:24:24 -0300

    Refactor vote types by removing id and userId from CreateVoteRequest and VoteResponse interfaces, streamlining the data structure for vote management.

--- src/types/vote.ts
+++ src/types/vote.ts
@@ -1,16 +1,13 @@
 // Creo el tipo de vote
 
 export interface CreateVoteRequest {
-  id: string;
   userId: string;
   playerId: string;
   matchId: string;
-  fechaVoto: Date;
 }
 
 export interface VoteResponse {
   id: string;
-  userId: string;
   playerId: string;
   matchId: string;
   fechaVoto: Date;
